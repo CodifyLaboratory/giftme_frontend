99c4a9d058aa8df561e31c4165f702c62bd3fbc0
Katalyzator <web.coder96@gmail.com>
2021-03-30 20:14:12 +0600

    MyWishList_Modal

--- src/Pages/MyWishList/MyWishList.jsx
+++ src/Pages/MyWishList/MyWishList.jsx
@@ -1,22 +1,86 @@
 import React from 'react'
-import WishlistContent from '../../components/wishlistContent/wishlistContent'
 import css from '../MyWishList/MyWishList.module.css'
-import {Link} from 'react-router-dom' 
+import MyWishContent from '../../components/MyWishContent/MyWishContent'
+import { Link } from 'react-router-dom'
+import plus from '../../Images/plus.png'
+import Modal from 'react-awesome-modal';
+import Pencil from '../../Images/pencil.png'
+import plus2 from '../../Images/plus2.png'
+
+
+
 
 
 
 function MyWishList() {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className={css.wrapper}>
-            <div>Мой список желаний</div>
-            <button>Добавь подарок</button>
-            <WishlistContent />
-            <WishlistContent />
-            <WishlistContent />
-            <WishlistContent />
-            <WishlistContent />
-            <Link className={css.more_Wish}>Показать еще...</Link>
+            <div className={css.title}>Мой список желаний</div>
+            <button onClick={handleOpen} className={css.btn}>
+                <img src={plus} alt="plus" />
+                <div>Добавь подарок</div>
+            </button>
+            <Modal
+                visible={open}
+                onClickAway={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div className={css.modal}>
+                    <div className={css.content}>
+                        <label htmlFor="nameOfProduct" className={css.label}>
+                            <div>Название подарка</div>
+                            <input type="text" id="nameOfProduct" />
+                            <img src={Pencil} alt="Pencil" />
+                        </label>
+                        <label htmlFor="description" className={css.label}>
+                            <div>Описание подарка</div>
+                            <input type="text" id="description" />
+                            <img src={Pencil} alt="Pencil" />
+                            <div className={css.symbol}>40 символов</div>
+                        </label>
+                        <label htmlFor="date" className={css.label}>
+                            <div>Дата</div>
+                            <input type="text" id="date" />
+                            <img src={Pencil} alt="Pencil" />
+                        </label>
+                        <label htmlFor="link" className={css.labelLink}>
+                            <div>Ссылка</div>
+                            <input type="text" id="link" />
+                            <img src={Pencil} alt="Pencil" />
+                        </label>
+                        <label htmlFor="foto" className={css.foto}>
+                            <div>Фото</div>
+                            <input type="file" id="foto" />
+                            <div className={css.inputFoto}>
+                                <img src={plus2} alt="plus" />
+                            </div>
+                        </label>
+                        <div className={css.btns}>
+                            <button>Сохранить</button>
+                            <button>Изменить</button>
+                        </div>
 
+                    </div>
+                </div>
+            </Modal>
+            <MyWishContent />
+            <MyWishContent />
+            <MyWishContent />
+            <MyWishContent />
+            <MyWishContent />
+            <Link className={css.more}>Показать еще...</Link>
         </div>
 
     )
--- src/pages/Wishlist/wishlist.jsx
+++ src/pages/Wishlist/wishlist.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import css from './wishlist.module.css'
 import WishlistContent from '../../components/wishlistContent/wishlistContent'
 import WishlistSidebar from '../../components/wishlistSidebar/wishlistSidebar';
-import {Link} from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 
 
 const Wishlist = () => {
     return (
         <div>
-        <div className={css.wishlist_content}>
-            <WishlistSidebar />
-            <div>
-                <h2 className={css.wishlist_title}>Список желаний</h2>
-                <WishlistContent />
-                <WishlistContent />
-                <WishlistContent />
-                <WishlistContent />
-                <WishlistContent />
-            </div>
+            <div className={css.wishlist_content}>
+                <WishlistSidebar />
+                <div>
+                    <h2 className={css.wishlist_title}>Список желаний</h2>
+                    <WishlistContent />
+                    <WishlistContent />
+                    <WishlistContent />
+                    <WishlistContent />
+                    <WishlistContent />
+                </div>
 
-        </div>
-        <Link className={css.more_Wish}>Показать еще...</Link>
+            </div>
+            <Link className={css.more_Wish}>Показать еще...</Link>
         </div>
     )
 }

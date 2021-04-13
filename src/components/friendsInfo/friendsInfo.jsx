53e0fe891a1239263c344b075e561c235018fabd
Kamila <bakytbekovakamila@gmail.com>
2021-04-13 18:16:28 +0300

    Last commit

--- src/components/friendsInfo/friendsInfo.jsx
+++ src/components/friendsInfo/friendsInfo.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './friendsInfo.module.css'
-import image from './../../img/image.png'
+import photo from './../../img/image.png'
+import Modal from 'react-awesome-modal'
+import { Link } from 'react-router-dom'
+
 
 const FriendsInfo = () => {
 
-return(
+    const [visible, setVisible] = useState(false)
 
- <div className={css.friends}>
- <div className={css.spans}>
-  <span></span>
-  <span></span>
-  <span></span>
- </div>
-  <img src={image} alt="image"/>
-  <p>Курманалиева Айгерим</p>
-  <input type="text" placeholder="Описание о себе"/>
-  
- </div>
+    const openMod = () => {
+        setVisible(true)
 
-)}
+    };
+
+    const closeMod = () => {
+        setVisible(false)
+
+    };
+
+    return (
 
 
-export  default FriendsInfo 
+        <div className={css.friends}>
+            <div className={css.spans}>
+                <span onClick={() => openMod()}></span>
+                <span onClick={() => openMod()}></span>
+                <span onClick={() => openMod()}></span>
+            </div>
+
+            <img src={photo} alt="" />
+
+            <p>Курманалиева Айгерим</p>
+            <input type="text" placeholder="Описание о себе" />
+
+            <Modal 
+                style={{color: 'red'}}
+                visible={visible}
+                width="276"
+                height="50"
+                effect="fadeInUp"
+                onClickAway={() => closeMod()}
+            >
+                <div className={css.modal}>
+                    <Link>Удалить друга</Link>
+                    <Link className={css.submit}>Добавить друга</Link>
+                </div>
+
+
+
+            </Modal>
+
+        </div>
+
+
+    )
+}
+
+
+export default FriendsInfo

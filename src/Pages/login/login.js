ac6cb432cd171388f9887bbfec688068cca54de5
Mdrkuz <kuzebaevtv@gmail.com>
2021-05-03 14:47:49 +0600

    split into components

--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -2,13 +2,10 @@ import css from './login.module.css'
 import axios from 'axios'
 import { useState } from 'react';
 import { useHistory } from 'react-router';
-<<<<<<< HEAD
-=======
 import { LogHead } from './components/logHead';
 import { EmailBlock } from './components/emailBlock';
 import { PasswordBlock } from './components/passwordBlock';
 import { ButtonBlock } from './components/buttonBlock';
->>>>>>> user
 
 
 function Login() {
@@ -29,39 +26,6 @@ function Login() {
       })
   }
 
-<<<<<<< HEAD
-
-  return (
-    <div>
-
-      <div class={css.Scene}>
-        <div class={css.page_container}>
-
-          <div class={css.login_form}>
-
-            <div class={css.title}>Авторизация</div>
-            <div class={css.title_description}>авторизация через
-            <span class={css.inner_title_description}>Gmail</span>
-            </div>
-            <div class={css.login_body}>
-              <div class={css.input_field_email}>
-                <div class={css.require_mark_email}>*</div>
-                <input value={email} onChange={(event) => setEmail(event.target.value)} class={css.inner_input_email} placeholder='Email' />
-              </div>
-              <div class={css.input_field_password}>
-                <div class={css.require_mark_password}>*</div>
-                <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} class={css.inner_input_password} placeholder="Пароль" />
-                <div class={css.input_password_description}>Не менее 8 символов.</div>
-              </div>
-              <div class={css.forgot_password}>Забыли пароль?</div>
-              <div class={css.button_block}>
-                <button onClick={submit} class={css.save_button}>
-                  <span class={css.save_button_label}>Войти</span>
-                </button>
-                <div class={css.button_block_description}>У вас еще нет аккаунта?</div>
-                <a href="#" class={css.registration_link}>Зарегистрироваться</a>
-              </div>
-=======
   return (
     <div>
 
@@ -82,7 +46,6 @@ function Login() {
 
               <ButtonBlock submit={submit}/>
 
->>>>>>> user
             </div>
 
           </div>
--- src/Pages/signUp/signUp.js
+++ src/Pages/signUp/signUp.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import css from './signUp.module.css'
-import axios from 'axios'
-import { useState } from 'react';
-import { useHistory } from 'react-router';
-=======
-import Pathes from "../../Utils/Pathes";
-import requester from "../../Utils/Requester";
+
 import css from './signUp.module.css'
 import { RegHead } from "./components/regHead";
 import { CheckBox } from "./components/checkBox";
@@ -15,21 +8,11 @@ import { useState } from 'react';
 import { useHistory } from 'react-router';
 import { PasswordBlock, RePasswordBlock } from "./components/passwordBlock";
 import { ButtonBlock } from "./components/buttonBlock";
->>>>>>> user
 
 function SignUp() {
 
   const history = useHistory()
   const [password, setPassword] = useState('');
-<<<<<<< HEAD
-  const [email, setEmail] = useState('');
-  const submit = (e) => {
-    e.preventDefault()
-    axios.post('http://167.99.46.0/auth/users/', {
-      email: email,
-      password: password
-    })
-=======
   const [rePassword, setRePassword] = useState('')
   const [email, setEmail] = useState('');
   
@@ -40,19 +23,15 @@ function SignUp() {
         email: email,
         password: password
       })
->>>>>>> user
       .then((res) => {
         history.push('/login')
       })
       .catch((err) => {
         alert(err.message);
       })
-<<<<<<< HEAD
-=======
     } else {
       alert('Пароли не совпадают!')
     }
->>>>>>> user
   }
 
   return (
@@ -61,39 +40,6 @@ function SignUp() {
         <div class={css.page_container}>
 
           <div class={css.registration_form}>
-<<<<<<< HEAD
-            <div class={css.title}>Регистрация</div>
-            <div class={css.title_description}>регистрация через
-            <span class={css.inner_title_description}>Gmail</span>
-            </div>
-            <form onSubmit={submit} class={css.registration_body}>
-              <div class={css.input_field_email}>
-                <div class={css.require_mark_email}>*</div>
-                <input type='email' required value={email} onChange={(event) => setEmail(event.target.value)} class={css.inner_input_email} placeholder="Email" />
-              </div>
-              <div class={css.input_field_password}>
-                <div class={css.require_mark_password}>*</div>
-                <input required type='password' value={password} onChange={(event) => setPassword(event.target.value)} class={css.inner_input_password} placeholder="Пароль" />
-                <div class={css.input_password_description}>Не менее 8 символов.</div>
-              </div>
-              <div class={css.input_field_re_password}>
-                <div class={css.require_mark_re_password}>*</div>
-                <input type='password' class={css.inner_input_re_password} placeholder="Подтвердить пароль" />
-              </div>
-              <input type="checkbox" required id='confirmBox' class={css.check_field} />
-              <label for="confirm" class={css.check_field_label}>я принимаю условия пользовательского соглашения</label>
-
-              <div class={css.button_block}>
-                <button class={css.save_button}>
-                  <span class={css.save_button_label}>Сохранить</span>
-                </button>
-                <a href="#" class={css.login_link}>Есть аккаунт?</a>
-              </div>
-
-            </form>
-          </div>
-
-=======
             <RegHead/>
             <form onSubmit={submit} class={css.registration_body}>
               <NameInput email={email} setEmail={setEmail}/>
@@ -115,7 +61,6 @@ function SignUp() {
 
             </form>
           </div>
->>>>>>> user
         </div>
       </div>
     </div>
